2dfcffca403269ba9f288872f02473b91d9226cc
Bassam123422 <203925550+Bassam123422@users.noreply.github.com>
2025-09-03 19:01:35 +0300

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -2,44 +2,54 @@ using UnityEngine;
 using AppodealAds.Unity.Api;
 using AppodealAds.Unity.Common;
 
-public class GameManager : MonoBehaviour, IRewardedVideoAdListener
+public class GameManager : MonoBehaviour, IInterstitialAdListener, IRewardedVideoAdListener
 {
-    string appKey = "9a4c0fbb6096be429f2de5a1c20e1cba160429c9698afbd5";
+    private string appKey = "9a4c0fbb6096be429f2de5a1c20e1cba160429c9698afbd5";
 
     void Start()
     {
-        int adTypes = AppodealAdType.Interstitial | AppodealAdType.Banner | AppodealAdType.RewardedVideo;
-
+        int adTypes = Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO;
         Appodeal.initialize(appKey, adTypes, true);
 
+        Appodeal.setInterstitialCallbacks(this);
         Appodeal.setRewardedVideoCallbacks(this);
     }
 
-    public void ShowInterstitial()
+    public void ShowAdOnGameOver()
     {
-        if (Appodeal.isLoaded(AppodealAdType.Interstitial))
+        if (Appodeal.isLoaded(Appodeal.INTERSTITIAL))
         {
-            Appodeal.show(AppodealShowStyle.Interstitial);
+            Appodeal.show(Appodeal.INTERSTITIAL);
         }
     }
 
-    public void ShowBanner()
+    public void ShowRewardedAd()
     {
-        Appodeal.show(AppodealShowStyle.BannerBottom);
-    }
-
-    public void ShowRewardedVideo()
-    {
-        if (Appodeal.isLoaded(AppodealAdType.RewardedVideo))
+        if (Appodeal.isLoaded(Appodeal.REWARDED_VIDEO))
         {
-            Appodeal.show(AppodealShowStyle.RewardedVideo);
+            Appodeal.show(Appodeal.REWARDED_VIDEO);
         }
     }
 
-    public void onRewardedVideoLoaded(bool isPrecache) { }
+    // Interstitial callbacks
+    public void onInterstitialLoaded(bool isPrecache) { }
+    public void onInterstitialFailedToLoad() { }
+    public void onInterstitialShown() { }
+    public void onInterstitialShowFailed() { }
+    public void onInterstitialClicked() { }
+    public void onInterstitialClosed() { }
+    public void onInterstitialExpired() { }
+
+    // Rewarded video callbacks
+    public void onRewardedVideoLoaded(bool precache) { }
     public void onRewardedVideoFailedToLoad() { }
     public void onRewardedVideoShown() { }
-    public void onRewardedVideoFinished(double amount, string name) { }
+    public void onRewardedVideoShowFailed() { }
+    public void onRewardedVideoFinished(double amount, string name)
+    {
+        Debug.Log("Reward granted: " + amount + " " + name);
+    }
     public void onRewardedVideoClosed(bool finished) { }
     public void onRewardedVideoExpired() { }
+    public void onRewardedVideoClicked() { }
 }
